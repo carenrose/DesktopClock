2d3c9f3fa6e76d6a97a93eeae79a04761471ea4b
Brianna Williams <brianna.k.williams@nebraska.gov>
2023-06-23 11:12:47 -0500

    Actual Initial commit :)

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTime24h = new System.Windows.Forms.Label();
+            this.lblTime12h = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime24h_Sec = new System.Windows.Forms.Label();
+            this.lblTime12h_Sec = new System.Windows.Forms.Label();
+            this.lblTime12h_ap = new System.Windows.Forms.Label();
+            this.lblDayOfWeek = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime24h
+            // 
+            this.lblTime24h.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime24h.Font = new System.Drawing.Font("Segoe UI Semibold", 28F, System.Drawing.FontStyle.Bold);
+            this.lblTime24h.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTime24h.Location = new System.Drawing.Point(0, 1);
+            this.lblTime24h.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTime24h.Name = "lblTime24h";
+            this.lblTime24h.Size = new System.Drawing.Size(128, 50);
+            this.lblTime24h.TabIndex = 0;
+            this.lblTime24h.Text = "00:00";
+            this.lblTime24h.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // lblTime12h
+            // 
+            this.lblTime12h.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime12h.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTime12h.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTime12h.Location = new System.Drawing.Point(2, 46);
+            this.lblTime12h.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTime12h.Name = "lblTime12h";
+            this.lblTime12h.Size = new System.Drawing.Size(90, 31);
+            this.lblTime12h.TabIndex = 0;
+            this.lblTime12h.Text = "0:00";
+            this.lblTime12h.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // lblDate
+            // 
+            this.lblDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.lblDate.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblDate.Location = new System.Drawing.Point(13, 98);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(163, 29);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "01/01/0000";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblTime24h_Sec
+            // 
+            this.lblTime24h_Sec.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime24h_Sec.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTime24h_Sec.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTime24h_Sec.Location = new System.Drawing.Point(115, -1);
+            this.lblTime24h_Sec.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTime24h_Sec.Name = "lblTime24h_Sec";
+            this.lblTime24h_Sec.Size = new System.Drawing.Size(68, 50);
+            this.lblTime24h_Sec.TabIndex = 0;
+            this.lblTime24h_Sec.Text = ":00";
+            this.lblTime24h_Sec.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblTime12h_Sec
+            // 
+            this.lblTime12h_Sec.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime12h_Sec.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTime12h_Sec.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTime12h_Sec.Location = new System.Drawing.Point(83, 46);
+            this.lblTime12h_Sec.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTime12h_Sec.Name = "lblTime12h_Sec";
+            this.lblTime12h_Sec.Size = new System.Drawing.Size(39, 31);
+            this.lblTime12h_Sec.TabIndex = 1;
+            this.lblTime12h_Sec.Text = ":00";
+            this.lblTime12h_Sec.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lblTime12h_ap
+            // 
+            this.lblTime12h_ap.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime12h_ap.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTime12h_ap.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTime12h_ap.Location = new System.Drawing.Point(117, 46);
+            this.lblTime12h_ap.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTime12h_ap.Name = "lblTime12h_ap";
+            this.lblTime12h_ap.Size = new System.Drawing.Size(59, 31);
+            this.lblTime12h_ap.TabIndex = 2;
+            this.lblTime12h_ap.Text = "pm";
+            this.lblTime12h_ap.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblDayOfWeek
+            // 
+            this.lblDayOfWeek.BackColor = System.Drawing.Color.Transparent;
+            this.lblDayOfWeek.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.lblDayOfWeek.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblDayOfWeek.Location = new System.Drawing.Point(13, 77);
+            this.lblDayOfWeek.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDayOfWeek.Name = "lblDayOfWeek";
+            this.lblDayOfWeek.Size = new System.Drawing.Size(163, 22);
+            this.lblDayOfWeek.TabIndex = 3;
+            this.lblDayOfWeek.Text = "Wednesday";
+            this.lblDayOfWeek.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(188, 127);
+            this.Controls.Add(this.lblDayOfWeek);
+            this.Controls.Add(this.lblTime12h_ap);
+            this.Controls.Add(this.lblTime12h_Sec);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTime12h);
+            this.Controls.Add(this.lblTime24h_Sec);
+            this.Controls.Add(this.lblTime24h);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.75D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime24h;
+        private System.Windows.Forms.Label lblTime12h;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime24h_Sec;
+        private System.Windows.Forms.Label lblTime12h_Sec;
+        private System.Windows.Forms.Label lblTime12h_ap;
+        private System.Windows.Forms.Label lblDayOfWeek;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Timer Timer { get; set; }
+
+        private Color InitialColor;
+        //private static Color InitialColor      = SystemColors.ControlLight;
+        private static Color FirstChangeColor  = Color.Goldenrod;
+        private static Color SecondChangeColor = SystemColors.Highlight;
+
+        private (int hour, int minute) FirstChangeTime;
+        private (int hour, int minute) SecondChangeTime;
+        private (int hour, int minute) ChangeBackTime;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.InitialColor = lblTime12h.ForeColor;
+
+            // position!
+            // https://stackoverflow.com/questions/1363374/showing-a-windows-form-on-a-secondary-monitor
+
+            var positionOnScreen = (Screen.AllScreens.Length > 1)
+                ? Screen.AllScreens.FirstOrDefault(s => s.DeviceName == @"\\.\DISPLAY1")        // the second monitor happens to be "\\.\DISPLAY1"
+                : Screen.PrimaryScreen;
+            if (positionOnScreen != null)
+            {
+                this.Top  = positionOnScreen.WorkingArea.Bottom - this.Height;
+                this.Left = positionOnScreen.WorkingArea.Right  - this.Width ;
+            }
+
+
+
+            #region Parse times from App.config
+
+            var change1 = ConfigurationManager.AppSettings.Get("FirstColorChangeTime" )?.Split(':');
+            var change2 = ConfigurationManager.AppSettings.Get("SecondColorChangeTime")?.Split(':');
+            var change3 = ConfigurationManager.AppSettings.Get("ChangeBackTime"       )?.Split(':');
+
+            // fallback to 16:30
+            this.FirstChangeTime = (16, 30);
+            if (change1 != null && change1.Length == 2)
+            {
+                if (int.TryParse(change1[0], out var hour) && int.TryParse(change1[1], out var mins))
+                {
+                    this.FirstChangeTime = (hour, mins);
+                }
+            }
+
+            // fallback to 17:00
+            this.SecondChangeTime = (17, 00);
+            if (change2 != null && change2.Length == 2)
+            {
+                if (int.TryParse(change2[0], out var hour) && int.TryParse(change2[1], out var mins))
+                {
+                    this.SecondChangeTime = (hour, mins);
+                }
+            }
+
+            // fallback to 20:00
+            this.ChangeBackTime = (20, 00);
+            if (change3 != null && change3.Length == 2)
+            {
+                if (int.TryParse(change3[0], out var hour) && int.TryParse(change3[1], out var mins))
+                {
+                    this.ChangeBackTime = (hour, mins);
+                }
+            }
+
+            #endregion
+
+            // timer
+            this.Timer = new Timer { Interval = 1000 };
+            this.Timer.Tick += Timer_Tick;
+            this.Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            lblTime24h  .Text = $"{now:HH:mm}";
+            lblTime12h  .Text = $"{now:h:mm}";
+            lblDate     .Text = $"{now:M/d/yyyy}";
+            lblDayOfWeek.Text = now.DayOfWeek.ToString();
+
+            lblTime24h_Sec.Text = $":{now:ss}";
+            lblTime12h_Sec.Text = $":{now:ss}";
+            lblTime12h_ap .Text = $"{now:tt}".ToLower();
+
+
+            #region Color changing
+
+            var withinFirstColorTime = now.Hour > this.FirstChangeTime .hour || (now.Hour == this.FirstChangeTime .hour && now.Minute >= this.FirstChangeTime .minute);
+            var withinSecndColorTime = now.Hour > this.SecondChangeTime.hour || (now.Hour == this.SecondChangeTime.hour && now.Minute >= this.SecondChangeTime.minute);
+            var afterChangeBackTime  = now.Hour > this.ChangeBackTime  .hour || (now.Hour == this.ChangeBackTime  .hour && now.Minute >= this.ChangeBackTime  .minute);
+
+            if (withinFirstColorTime && !withinSecndColorTime && !afterChangeBackTime)
+            {
+                // change to first color
+                if (lblTime12h.ForeColor != FirstChangeColor)
+                {
+                    lblTime12h.ForeColor     = FirstChangeColor;
+                    lblTime12h_Sec.ForeColor = FirstChangeColor;
+                    lblTime12h_ap.ForeColor  = FirstChangeColor;
+                }
+            }
+            else if (withinSecndColorTime && !afterChangeBackTime)
+            {
+                // change to second color
+                if (lblTime12h.ForeColor != SecondChangeColor)
+                {
+                    lblTime12h.ForeColor     = SecondChangeColor;
+                    lblTime12h_Sec.ForeColor = SecondChangeColor;
+                    lblTime12h_ap.ForeColor  = SecondChangeColor;
+                }
+            }
+            else
+            {
+                // if color is not initial, change back to initial
+                if (lblTime12h.ForeColor != InitialColor)
+                {
+                    lblTime12h.ForeColor     = InitialColor;
+                    lblTime12h_Sec.ForeColor = InitialColor;
+                    lblTime12h_ap.ForeColor  = InitialColor;
+                }
+            }
+
+            /*
+            // change at 1630
+            if (lblTime12h.ForeColor == this.InitialColor && now.Hour >= 16 && now.Minute >= 30)
+            {
+                lblTime12h.ForeColor     = FirstChangeColor;
+                lblTime12h_Sec.ForeColor = FirstChangeColor;
+                lblTime12h_ap.ForeColor  = FirstChangeColor;
+            }
+            // change at 1700
+            else if (lblTime12h.ForeColor != SecondChangeColor && now.Hour >= 17)
+            {
+                lblTime12h.ForeColor     = SecondChangeColor;
+                lblTime12h_Sec.ForeColor = SecondChangeColor;
+                lblTime12h_ap.ForeColor  = SecondChangeColor;
+            }
+            // change back
+            else if (lblTime12h.ForeColor != this.InitialColor && now.Hour >= 20)
+            {
+                lblTime12h.ForeColor     = this.InitialColor;
+                lblTime12h_Sec.ForeColor = this.InitialColor;
+                lblTime12h_ap.ForeColor  = this.InitialColor;
+            }
+            */
+            #endregion
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.Timer?.Enabled ?? false)
+            {
+                this.Timer.Stop();
+                this.Timer.Dispose();
+            }
+        }
+    }
+}
